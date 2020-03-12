06560a97d2dae725c47c9d2a4804a22c33d6d099
sh39works <sh39works@gmail.com>
2020-03-13 03:12:20 +0900

    Update setSignedCookie.php

--- src/sh39AWSLib/setSignedCookie.php
+++ src/sh39AWSLib/setSignedCookie.php
@@ -4,7 +4,48 @@ namespace sh39\AWSLib ;
 
 class setSignedCookie
 {
-	function set() {
-		
+	private $expire ;
+	private $resourceKey ;
+	private $remoteAddress ;
+	private $signedCookie ;
+	private $domain ;
+	private $privateKey ;
+	private $keyPairId ;
+
+	function __construct(array $args){
+		$this->expire        = $args['expire'] ;
+		$this->resourceKey   = $args['resourceKey'] ;
+		$this->remoteAddress = $_SERVER['REMOTE_ADDR'] ;
+		$this->signedCookie  = $args['signedCookie'] ;
+		$this->domain        = ".".$args['domain'] ;
+		$this->privateKey    = $args['privateKey'] ;
+		$this->keyPairId     = $args['keyPairId'] ;
+	}
+	function customPolicy(){
+		return <<<POLICY
+		{
+		    "Statement": [
+		        {
+		            "Resource": "{$this->resourceKey}",
+		            "Condition": {
+		                "IpAddress": {"AWS:SourceIp": "{$this->remoteAddress}/32"},
+		                "DateLessThan": {"AWS:EpochTime": {$this->expires}}
+		            }
+		        }
+		    ]
+		}
+		POLICY;
+	}
+	function signedCookie(){
+		$signedCookieCustomPolicy = $cloudFront->getSignedCookie([
+			"policy" => customPolicy(),
+			"private_key" => $this->privateKey,
+			"key_pair_id" => $this->keyPairId
+		]);
+	}
+	function set(){
+		foreach ($this->signedCookie as $name => $value) {
+			setcookie($name, $value, 0, "", $this->domain, true, true);
+		}
 	}
 }
