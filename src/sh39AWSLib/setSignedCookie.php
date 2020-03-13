a06441a8a6005da3f1d291a0493d1b21355d1a42
sh39works <sh39works@gmail.com>
2020-03-13 15:39:55 +0900

    200313 1539

--- src/sh39AWSLib/setSignedCookie.php
+++ src/sh39AWSLib/setSignedCookie.php
@@ -1,23 +1,38 @@
 <?php
-
 namespace sh39\AWSLib ;
 
-class setSignedCookie
+// AWS SDK V3 必須
+
+use Aws\CloudFront\CloudFrontClient as cloudfront;
+
+/*
+必須変数
+$expire : cookieの有効期限（設定しない場合、15分）
+$resourceKey : 証明書を有効にするファイル or ディレクトリ
+$domain : 証明書を有効にするドメイン（ルートドメインの場合、最初に.をつける）
+$privateKey : SSHプライベートキーの絶対パス
+$keyPairId : A******************QのIP名
+
+その他
+$remoteAddress : 現在閲覧しているIPアドレス
+*/
+
+class setSignedCookie extends cloudfront
 {
 	private $expire ;
 	private $resourceKey ;
 	private $remoteAddress ;
-	private $signedCookie ;
 	private $domain ;
 	private $privateKey ;
 	private $keyPairId ;
 
 	function __construct(array $args){
-		$this->expire        = $args['expire'] ;
+		$this->expire        = ( !empty( $args['expire'] ))
+							 ? $args['expire']
+							 : time()+60*15 ;
 		$this->resourceKey   = $args['resourceKey'] ;
 		$this->remoteAddress = $_SERVER['REMOTE_ADDR'] ;
-		$this->signedCookie  = $args['signedCookie'] ;
-		$this->domain        = ".".$args['domain'] ;
+		$this->domain        = $args['domain'] ;
 		$this->privateKey    = $args['privateKey'] ;
 		$this->keyPairId     = $args['keyPairId'] ;
 	}
@@ -37,14 +52,14 @@ class setSignedCookie
 		POLICY;
 	}
 	function signedCookie(){
-		$signedCookieCustomPolicy = $cloudFront->getSignedCookie([
-			"policy" => customPolicy(),
+		return $cloudFront->getSignedCookie([
+			"policy" => $this->customPolicy(),
 			"private_key" => $this->privateKey,
 			"key_pair_id" => $this->keyPairId
 		]);
 	}
 	function set(){
-		foreach ($this->signedCookie as $name => $value) {
+		foreach ($this->signedCookie() as $name => $value) {
 			setcookie($name, $value, 0, "", $this->domain, true, true);
 		}
 	}
